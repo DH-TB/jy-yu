c3eeecbe5376a52f89717aa991360386f9ed1f6b
lizhen <lzhuang@thoughtworks.com>
2024-09-02 00:20:20 +0800

    [lizhen] feat: add search input

--- src/pages/function/pages/call/index.tsx
+++ src/pages/function/pages/call/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, Image } from '@tarojs/components'
+import React, { useMemo, useState } from 'react'
+import { View, Text, Image, Input } from '@tarojs/components'
 import styles from './index.module.scss'
 import cx from 'classnames';
 import Taro from '@tarojs/taro'
@@ -7,6 +7,10 @@ import { defaultDarkBackground, randomColor } from '../../../../constants/color'
 import { CALLS, CALLS_COMMON, CALLS_OTHER } from '../../../../constants/call';
 
 const Call = (props: { activeTabIndex: number }) => {
+  const [searchText, setSearchText] = useState('')
+  const callOthers = useMemo(() => searchText === '' ? CALLS_OTHER :
+    CALLS_OTHER.filter((item) => item.song.toLocaleUpperCase().includes(searchText.toLocaleUpperCase())
+    ), [searchText]);
 
   const copy = (data) => {
     Taro.setClipboardData({
@@ -19,7 +23,7 @@ const Call = (props: { activeTabIndex: number }) => {
       {
         CALLS.map((item) =>
           <View className={cx(styles.songCard, 'at-col')} onClick={() => copy(item.text)} >
-            <Image className={cx(styles.songCard, 'at-col', styles.songBackground)} style={{ background: defaultDarkBackground }}src={item.image}></Image>
+            <Image className={cx(styles.songCard, 'at-col', styles.songBackground)} style={{ background: defaultDarkBackground }} src={item.image}></Image>
             <View className={cx(styles.songCardText)}>
               <Text className={cx(styles.title, styles.text)}>{item.song}</Text>
               <Text className={styles.text}>{item.text}</Text>
@@ -31,8 +35,13 @@ const Call = (props: { activeTabIndex: number }) => {
     </View>
   } else if (props.activeTabIndex === 1) {
     return <View className={cx(styles.cardWrap)}>
+      <View className={styles.inputWrap}>
+        <View className={cx(styles.inputIcon, 'at-icon at-icon-search')} />
+        <Input placeholder='请输入' className={styles.input} value={searchText} onInput={(data) => setSearchText(data.detail.value)} />
+      </View>
+
       {
-        CALLS_OTHER.map((item) => (
+        callOthers.map((item) => (
           <View className={cx(styles.songCard, styles.otherSongCard)} onClick={() => copy(item.text)} >
             <View className={cx(styles.songCardText, styles.otherSongCard)} style={{ background: item.background }}>
               <Text className={cx(styles.otherSongTitle)}>{item.song}</Text>
